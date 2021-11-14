5a73862ec3867381adb8f0188cf383a42bea4121
jeissongarciacano <jei.isson.1999@gmail.com>
2021-11-14 08:33:47 -0500

    make it better

--- website/test_selenium.py
+++ website/test_selenium.py
@@ -1,2 +1,10 @@
+from selenium import webdriver
+
+driver = webdriver.Edge("C:\\Users\\jeiis\\Downloads\\edgedriver_win64\\msedgedriver.exe")
+
+driver.get('https://webappjg.herokuapp.com/')
+
+driver.find_element_by_id("b1").click()
+
 
 
