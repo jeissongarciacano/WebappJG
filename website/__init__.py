e73de789662ce2260f7e820d8193c7084ed8eedb
jeissongarciacano <jei.isson.1999@gmail.com>
2021-11-13 22:29:12 -0500

    make it better

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app(): 
     app= Flask(__name__)
-    ## app.config['SECRET_KEY']= 'ggg555'
+    app.config['SECRET_KEY']= 'ggg555'
     from .views import views
     app.register_blueprint(views, url_prefix='/')
     return app
