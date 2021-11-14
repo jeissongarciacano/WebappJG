07f7c73768512ccbb97e6d35f5a2ac9c0da647c8
jeissongarciacano <jei.isson.1999@gmail.com>
2021-11-14 08:46:45 -0500

    make it better

--- website/test_selenium.py
+++ website/test_selenium.py
@@ -5,6 +5,23 @@ driver = webdriver.Edge("C:\\Users\\jeiis\\Downloads\\edgedriver_win64\\msedgedr
 driver.get('https://webappjg.herokuapp.com/')
 
 driver.find_element_by_id("b1").click()
+driver.find_element_by_id("home").click()
+driver.find_element_by_id("b2").click()
+driver.find_element_by_id("home").click()
+driver.find_element_by_id("b3").click()
+driver.find_element_by_id("home").click()
+driver.find_element_by_id("b4").click()
+driver.find_element_by_id("home").click()
+driver.find_element_by_id("b5").click()
+driver.find_element_by_id("home").click()
+driver.find_element_by_id("Fibonacci").click()
+
+driver.find_element_by_id("Factorial").click()
+driver.find_element_by_id("APtriangulo").click()
+driver.find_element_by_id("APrectangulo").click()
+driver.find_element_by_id("APcirculo").click()
+
+
 
 
 
